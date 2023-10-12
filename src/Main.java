4bece0800ce9bedb7f803825a783299b13f025ea
Abdullah Nadim <abdullahnadim.15@yahoo.com>
2023-10-13 00:20:11 +0200

    first commit testing

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-
+        System.out.println("first commit");
         System.out.println("testing commit");
     }
 }
