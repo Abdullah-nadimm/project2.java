182111b7d022f456e7ff423919e34882aa2ae70c
Abdullah Nadim <abdullahnadim.15@yahoo.com>
2023-10-13 00:15:20 +0200

    deleting commit testing

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("first commit");
+
         System.out.println("testing commit");
+
     }
 }
